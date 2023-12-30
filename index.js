15e6c6462d6a9347bf57056936c4cbf1ec091d1e
chefboyrd53 <143761455+chefboyrd53@users.noreply.github.com>
2023-12-29 23:25:51 -0500

    chatgpt fix

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ var allTracks = [new struct('mariokartstadium', 0, 0),
                  new struct('thwompruins', 0, 0)];
 
 
-function test(track, placement){
-    for(let i = 0; i < 4; i++){
-        if(allTracks[i].track == track){
+function test(track, placement) {
+    for (let i = 0; i < 4; i++) {
+        if (allTracks[i].track == track) {
             allTracks[i].timesPlayed++;
-            allTracks[i].avgFinish = ((allTracks[i].avgFinish * (timesPlayed - 1)) + placement) / timesPlayed;
-            document.getElementById("demo").innerHTML = track + '\nAvgerage Placement: ' + avgFinish + '\nTimes Played: ' + timesPlayed;
+            allTracks[i].avgFinish = ((allTracks[i].avgFinish * (allTracks[i].timesPlayed - 1)) + placement.value) / allTracks[i].timesPlayed;
         }
     }
+
+    // Update the demo paragraph after the loop
+    document.getElementById("demo").innerHTML = track + '<br>Avgerage Placement: ' + allTracks[i].avgFinish + '<br>Times Played: ' + allTracks[i].timesPlayed;
 }
