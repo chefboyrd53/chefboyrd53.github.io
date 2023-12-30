ccf9316bb2662a51f24bfa36eb6ac4eda5c4dcd8
chefboyrd53 <143761455+chefboyrd53@users.noreply.github.com>
2023-12-29 23:18:21 -0500

    struct + new test function

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function struct(track, avgFinish, timesPlayed) {
+    this.track = track;
+    this.avgFinish = avgFinish;
+    this.timesPlayed = timesPlayed;
+}
+
+var allTracks = [new struct('mariokartstadium', 0, 0),
+                 new struct('waterpark', 0, 0),
+                 new struct('sweetsweetcanyon', 0, 0),
+                 new struct('thwompruins', 0, 0)];
+
+
 function test(track, placement){
-    alert(track + '\n' + placement.value);
+    for(let i = 0; i < 4; i++){
+        if(allTracks[i].track == track){
+            allTracks[i].timesPlayed++;
+            allTracks[i].avgFinish = ((allTracks[i].avgFinish * (timesPlayed - 1)) + placement) / timesPlayed;
+            document.getElementById("demo").innerHTML = track + '\nAvgerage Placement: ' + avgFinish + '\nTimes Played: ' + timesPlayed;
+        }
+    }
 }
